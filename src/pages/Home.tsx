5182da7717192886d22fadedaec5d1ba37d8a1f4
HungDang48 <gologolo4567@gmail.com>
2025-05-05 13:02:21 +0700

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './Home.css';
+import Banner from '../admin/component/Banner/Banner';
 
 const Home = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -10,7 +11,8 @@ const Home = () => {
 
   return (
     <div className="main-content">
-      <video 
+      <Banner/>
+      {/* <video 
         className="background-video"
         autoPlay 
         loop 
@@ -19,26 +21,26 @@ const Home = () => {
       >
         <source src="/video/wodo video 3 .mp4" type="video/mp4" />
 
-      </video>
+      </video> */}
 
-      <div className="center-content">
+      {/* <div className="center-content">
         <h1 className={`title ${isVisible ? 'visible' : ''}`}>WODO</h1>
         <div className={`sub-title ${isVisible ? 'visible' : ''}`}>
           <p className="item">WOULD THINKS</p>
           <p className="item">
           WOULD DO</p>
         </div>
-      </div>
+      </div> */}
 
-      <div className={`left-footer-group ${isVisible ? 'visible' : ''}`}>
+      {/* <div className={`left-footer-group ${isVisible ? 'visible' : ''}`}>
         <p className="footer-text">PATHFINDER SCOUTS VIETNAM </p>
         <p className="footer-subtext"></p>
-      </div>
+      </div> */}
 
-      <div className={`right-footer-group ${isVisible ? 'visible' : ''}`}>
+      {/* <div className={`right-footer-group ${isVisible ? 'visible' : ''}`}>
         <p className="side-text">BETTER WORLD CAMP 2025</p>
         <p className="footer-subtext">vì tha nhân, vì một xã hội tốt đẹp</p>
-      </div>
+      </div> */}
     </div>
   );
 };
