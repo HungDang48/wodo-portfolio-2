35012c743f08ef957a5d1f695530f5d2e2ae919d
Đặng Việt Hưng <gologolo4567@gmail.com>
2025-06-17 15:41:10 +0700

    Update DonatePage.tsx webhook n8n

--- src/pages/Donate/DonatePage.tsx
+++ src/pages/Donate/DonatePage.tsx
@@ -33,7 +33,7 @@ const DonatePage: React.FC = () => {
     setIsSubmitting(true);
 
     try {
-      const res = await fetch("https://wodo-thanks-letter.onrender.com", {
+      const res = await fetch("https://huwgdang.app.n8n.cloud/webhook/test", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
